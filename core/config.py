f20e10f24779b1bfb36547424930beb4dad92a39
ivankorolev <ikorolev368@gmail.com>
2026-01-23 01:13:15 +0300

    finish my simple API!!!!!

---
+++ core/config.py
@@ -0,0 +1,8 @@
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    api_v1_prefix: str = "/api/v1"
+    db_url: str = "sqlite+aiosqlite:///./shop.db"
+
+settings = Settings()
+
--- main.py
+++ main.py
@@ -1,10 +1,24 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 import uvicorn
+
+from core.models import Base, db_helper
+
+from core.config import settings
 from items_views import router as items_router
 from users.views import router as users_router
+from api_v1 import router as router_v1
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with db_helper.engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    
+    yield
 
 
-app = FastAPI()
+app = FastAPI(lifespan=lifespan)
+app.include_router(router= router_v1, prefix=settings.api_v1_prefix)
 app.include_router(items_router, tags=["items"])
 app.include_router(users_router)
 
@@ -34,4 +48,5 @@ def add(a: int, b : int):
 
 
 if __name__ == '__main__':
+
     uvicorn.run("main:app", reload=True)
